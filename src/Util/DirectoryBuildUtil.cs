48d5384cec51fef7c36c84d0f940899ac1c7e10e
Bob Chin <cnsharp@foxmail.com>
2018-04-16 01:04:05 +0800

    init

---
+++ src/Util/DirectoryBuildUtil.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using CnSharp.VisualStudio.Extensions.Projects;
+using EnvDTE;
+
+namespace CnSharp.VisualStudio.NuPack.Util
+{
+    public static class DirectoryBuildUtil
+    {
+        public static DirectoryBuildProps GetDirectoryBuildProps(this Solution solution)
+        {
+            var file = Path.Combine(Path.GetDirectoryName(solution.FullName), DirectoryBuildProps.FileName);
+            return !File.Exists(file) ? null : new DirectoryBuildProps(file);
+        }
+    }
+
+    public class DirectoryBuildProps : IPackageCommonMetadata
+    {
+        private readonly string _file;
+
+        public DirectoryBuildProps()
+        {
+            
+        }
+
+        public DirectoryBuildProps(string file)
+        {
+            _file = file;
+            if(!File.Exists(file)) throw new FileNotFoundException("not found",file);
+            var doc = new XmlDocument();
+            doc.Load(_file);
+            var parent = doc.ChildNodes[0].ChildNodes[0];
+            var props = GetType().GetProperties().ToList();
+            foreach (XmlNode node in parent.ChildNodes)
+            {
+                var prop = props.Find(m => m.Name == node.Name);
+                if (prop == null) continue;
+                prop.SetValue(this,node.InnerXml,null);
+            }
+        }
+
+        public void Save()
+        {
+            if (_file == null)
+                return;
+            SaveTo(_file);
+        }
+
+        public void SaveTo(string file)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(XmlTemplate);
+            var tempNode = doc.CreateElement("temp");
+            tempNode.InnerXml = XmlSerializerHelper.GetXmlStringFromObject(this);
+            doc.ChildNodes[0].ChildNodes[0].InnerXml = tempNode.ChildNodes[0].InnerXml;
+            doc.Save(file);
+        }
+
+        public IEnumerable<string> GetValuedProperties()
+        {
+            var props = GetType().GetProperties().ToList();
+            foreach (var prop in props)
+            {
+                var v = prop.GetValue(this);
+                if (!string.IsNullOrWhiteSpace(v?.ToString()))
+                    yield return prop.Name;
+            }
+        }
+
+        public const string FileName = "Directory.Build.props";
+        private const string XmlTemplate = @"<Project>
+  <PropertyGroup>
+  </PropertyGroup>
+</Project>";
+
+        #region Implementation of IPackageCommonMetadata
+
+        public string Version { get; set; }
+        public string Copyright { get; set; }
+        public string Product { get; set; }
+        public string Company { get; set; }
+        public string Trademark { get; set; }
+        public string Authors { get; set; }
+        public string Owners { get; set; }
+
+        #endregion
+    }
+}
