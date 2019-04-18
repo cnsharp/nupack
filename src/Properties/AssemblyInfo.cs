d002e40302a628c86b094a7b69dd8a63435439b3
bobchin <cnsharp@foxmail.com>
2019-04-18 23:06:19 +0800

    VS2019 support

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CnSharp Studio")]
 [assembly: AssemblyProduct("NuPack")]
-[assembly: AssemblyCopyright("Copyright © CnSharp Studio 2013-2018")]
+[assembly: AssemblyCopyright("Copyright © CnSharp Studio 2013-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
